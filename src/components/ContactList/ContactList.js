840a9afcba0bb5789dd9b58c3225ea5152b643d2
KhrystynaLavryniv <84865514+KhrystynaLavryniv@users.noreply.github.com>
2022-06-20 14:26:03 +0200

    fixed contacts ul

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,10 +1,35 @@
-import ContactItem from './ContactItem';
 import { Contacts } from './ContactList.style';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeContacts } from '../../redux/contactsSlice';
+import { Item, ItemBtn } from './ContactList.style';
 
-const ContactList = () => (
-  <Contacts>
-    <ContactItem />
-  </Contacts>
-);
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
+
+  const filterVisibleContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+  const handlerContactDelete = name => {
+    dispatch(removeContacts(contacts.filter(contact => name !== contact.name)));
+  };
+  return (
+    <>
+      <Contacts>
+        {filterVisibleContacts().map(contact => (
+          <Item key={contact.id}>
+            {contact.name}: {contact.number}
+            <ItemBtn onClick={() => handlerContactDelete(contact.name)}>
+              Delete
+            </ItemBtn>
+          </Item>
+        ))}
+      </Contacts>
+    </>
+  );
+};
 
 export default ContactList;
--- src/components/ContactList/ContactList.style.jsx
+++ src/components/ContactList/ContactList.style.jsx
@@ -8,4 +8,14 @@ export const Contacts = styled.label`
 export const Item = styled.li`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 5px;
+`;
+
+export const ItemBtn = styled.button`
+  margin-left: 5px;
+  cursor: pointer;
+  &:hover,
+  :focus {
+    background-color: #0066cc;
+  }
 `;
